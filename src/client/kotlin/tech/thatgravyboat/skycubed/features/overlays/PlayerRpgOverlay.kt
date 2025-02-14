79a1c5a9fc18f1a0218501d5158e18cbc82cc258
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-02-14 20:42:02 +0100

    move to separate sprites
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/client/kotlin/tech/thatgravyboat/skycubed/features/overlays/PlayerRpgOverlay.kt
+++ src/client/kotlin/tech/thatgravyboat/skycubed/features/overlays/PlayerRpgOverlay.kt
@@ -12,10 +12,11 @@ import tech.thatgravyboat.skyblockapi.utils.text.Text
 import tech.thatgravyboat.skycubed.SkyCubed
 import tech.thatgravyboat.skycubed.api.overlays.Overlay
 import tech.thatgravyboat.skycubed.config.overlays.OverlayPositions
-import tech.thatgravyboat.skycubed.config.overlays.Position
 import tech.thatgravyboat.skycubed.config.overlays.OverlaysConfig
+import tech.thatgravyboat.skycubed.config.overlays.Position
 import tech.thatgravyboat.skycubed.utils.blitSpritePercentX
 import tech.thatgravyboat.skycubed.utils.drawScaledString
+
 
 private const val WIDTH = 119
 private const val HEIGHT = 48
@@ -23,6 +24,12 @@ private const val HEIGHT = 48
 object PlayerRpgOverlay : Overlay {
 
     private val BASE = SkyCubed.id("rpg/base")
+    private val MAIN_BACKGROUND = SkyCubed.id("rpg/base/main_background")
+    private val BACKGROUND_OUTLINE = SkyCubed.id("rpg/base/background_outline")
+    private val BARS = SkyCubed.id("rpg/base/bars")
+    private val LEVEL = SkyCubed.id("rpg/base/level")
+    private val PERSON = SkyCubed.id("rpg/base/person")
+
     private val HEALTH = SkyCubed.id("rpg/health/normal")
     private val ABSORPTION = SkyCubed.id("rpg/health/absorption")
     private val MANA = SkyCubed.id("rpg/mana/normal")
@@ -48,7 +55,16 @@ object PlayerRpgOverlay : Overlay {
         val airPercent = McPlayer.air.toFloat() / McPlayer.maxAir.toFloat()
         val manaUsePercent = (McPlayer.heldItem.getData(DataTypes.RIGHT_CLICK_MANA_ABILITY)?.second?.toFloat() ?: 0f) / StatsAPI.maxMana.toFloat()
 
-        graphics.blitSprite(RenderType::guiTextured, BASE, 0, 0, WIDTH, HEIGHT)
+        //graphics.blitSprite(RenderType::guiTextured, BASE, 0, 0, WIDTH, HEIGHT)
+
+        graphics.blitSprite(RenderType::guiTextured, MAIN_BACKGROUND, 6, 3, 38, 41)
+        graphics.blitSprite(RenderType::guiTextured, BACKGROUND_OUTLINE, 3, 0, 44, 47)
+
+        graphics.blitSprite(RenderType::guiTextured, PERSON, 11, 13, 28, 31)
+
+        graphics.blitSprite(RenderType::guiTextured, LEVEL, 0, 26, 22, 22)
+        graphics.blitSprite(RenderType::guiTextured, BARS, 47, 16, 72, 19)
+
         graphics.blitSpritePercentX(HEALTH, 47, 23, 70, 5, healthPercent.coerceIn(0f, 1f))
         graphics.blitSpritePercentX(ABSORPTION, 47, 23, 70, 5, absorptionPercent.coerceIn(0f, 1f))
         graphics.blitSpritePercentX(MANA_DEPLETED, 47, 18, 57, 4, manaUsePercent.coerceIn(0f, 1f))
